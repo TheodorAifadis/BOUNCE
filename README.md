451929995e945097d795e35c9f21cf66bd28dde5
TheodorAifadis <theodor.aifadis@skola.botkyrka.se>
2021-02-15 16:06:04 +0100

    first-body

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-spel liknande geometry dash
-boll som rullar framåt
-hinder på banan
-olika plan att hoppa mellan
+Idéer till hur spelet ska se ut:
+
+Spel liknande geometry dash
+Boll som rullar framåt
+Hinder på banan
+Olika plan att hoppa mellan
+
+Milstolpar:
+1 Skapa en plattform som bollen är på (skapa en body)
+2 Få plattformen att röra på sig / få bollen att röra sig
+3 Lägg till hinder
+4 Score / highscore
+
+Framgång veckovis:
+v.7 Skapat en plattform med massa, när bollen kommer i kontakt med plattformen så tar det stopp och bollen ligger på plattformen. 
---
+++ main.lua
@@ -0,0 +1,48 @@
+function love.load()
+    -- Det här sätter 1 meter till 64 pixlar
+    love.physics.setMeter(64)
+    -- Det här ger "världen" gravitation, 9.82*64 är tyngkraften multiplicerat med 1 meter i pixlar
+    world = love.physics.newWorld(0, 9.82*64, true)
+
+  -- Table som kommer innehålla alla fysiska objekt
+    objects = {}
+
+  -- Det här skapar en plattform/marken
+    objects.ground = {}
+  -- Marken är en rektangel som utgår från centrum vilket gör att jag tar 450/2, 450-50/2 för att få den längre ner
+    objects.ground.body = love.physics.newBody(world, 450/2, 450-50/2)
+  -- Här skapas en rektangel med bredden 450 och höjden 50
+    objects.ground.shape = love.physics.newRectangleShape(1000, 10)
+  -- Det här sätter ihop rektangeln med bodyn
+    objects.ground.fixture = love.physics.newFixture(objects.ground.body,objects.ground.shape)
+
+  -- Här skapas en boll
+  objects.ball = {}
+  -- Detta palcerar bollen i centrum
+  objects.ball.body = love.physics.newBody(world, 650/2, 450/2, "dynamic")
+  -- Boll med radien 20
+  objects.ball.shape = love.physics.newCircleShape(20)
+  -- Det här lägger till i bodyn och 1 på slutet ger den bollen densiteten 1
+  objects.ball.fixture = love.physics.newFixture(objects.ball.body,objects.ball.shape, 1)
+
+  -- Gör bakgrunden vit
+  love.graphics.setBackgroundColor(1, 1, 1)
+  -- Det här bestämmer love-fönstrets dimensioner
+  love.window.setMode(650, 650)
+end
+
+function love.update(dt)
+    -- Det här ger världen dess rörelse
+    world:update(dt)
+end
+   
+  function love.draw()
+    -- Gör plattformen svart
+    love.graphics.setColor(0, 0, 0)
+    -- Det här ritar polygonen som är marken
+    love.graphics.polygon("fill", objects.ground.body:getWorldPoints(objects.ground.shape:getPoints()))
+   
+    -- Gör bollen grön
+    love.graphics.setColor(0, 0.3, 0.4)
+    love.graphics.circle("fill", objects.ball.body:getX(),objects.ball.body:getY(), objects.ball.shape:getRadius())
+end
