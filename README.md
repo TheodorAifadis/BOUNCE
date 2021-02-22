8a2c6630d5f5710104d1927888f2a2cd26bfe9c8
TheodorAifadis <theodor.aifadis@skola.botkyrka.se>
2021-02-22 16:39:21 +0100

    commit-3

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
-Idéer till hur spelet ska se ut:
-
-Spel liknande geometry dash
-Boll som rullar framåt
-Hinder på banan
-Olika plan att hoppa mellan
+Hur ska spelet se ut?
+    Boll som studsar som kan styras i sidleds av spelaren
+    Föremål som faller från himlen som ska undvikas
 
 Milstolpar:
-1 Skapa en plattform som bollen är på (skapa en body)
-2 Få plattformen att röra på sig / få bollen att röra sig
-3 Lägg till hinder
+1 Skapa en plattform som bollen är på (skapa en body)       KLAR
+2 Få bollen att röra sig
+3 Lägg till objekt som faller
 4 Score / highscore
 
 Framgång veckovis:
-v.7 Skapat en plattform med massa, när bollen kommer i kontakt med plattformen så tar det stopp och bollen ligger på plattformen. 
+v.7 Skapat en plattform med massa
+v.8 När bollen kommer i kontakt med marken så studsar den upp igen
+    Bollen går att styras i sidleds
+    "Väggar", bollen försvinner inte från skärmen
--- main.lua
+++ main.lua
@@ -1,48 +1,61 @@
 function love.load()
-    -- Det här sätter 1 meter till 64 pixlar
-    love.physics.setMeter(64)
-    -- Det här ger "världen" gravitation, 9.82*64 är tyngkraften multiplicerat med 1 meter i pixlar
-    world = love.physics.newWorld(0, 9.82*64, true)
+  -- En meter i spelvärlden är 64 pixlar
+  love.physics.setMeter(64)
+  -- Det här ger världen en allmän gravitation som endast verkar på det vertikala planet med 9.82
+  world = love.physics.newWorld(0, 9.82*64, true)
+ 
+  objects = {} -- Här skapar jag en table där att fysiska kroppar kommer finnas
+ 
+  -- Skapar marken som är en rektangel som placeras centralt långt ner på skärmen
+  objects.ground = {}
+  objects.ground.body = love.physics.newBody(world, 650/2, 650-50/2)
+  objects.ground.shape = love.physics.newRectangleShape(650, 50)
+  objects.ground.fixture = love.physics.newFixture(objects.ground.body, objects.ground.shape)
 
-  -- Table som kommer innehålla alla fysiska objekt
-    objects = {}
+  objects.leftWall = {}
+  objects.leftWall.body = love.physics.newBody(world, 0, 650-50/2)
+  objects.leftWall.shape = love.physics.newRectangleShape(1, 650)
+  objects.leftWall.fixture = love.physics.newFixture(objects.leftWall.body, objects.leftWall.shape)
 
-  -- Det här skapar en plattform/marken
-    objects.ground = {}
-  -- Marken är en rektangel som utgår från centrum vilket gör att jag tar 450/2, 450-50/2 för att få den längre ner
-    objects.ground.body = love.physics.newBody(world, 450/2, 450-50/2)
-  -- Här skapas en rektangel med bredden 1000 och höjden 10 (px)
-    objects.ground.shape = love.physics.newRectangleShape(1000, 10)
-  -- Det här sätter ihop rektangeln med bodyn
-    objects.ground.fixture = love.physics.newFixture(objects.ground.body,objects.ground.shape)
-
-  -- Här skapas en boll
+  objects.rightWall = {}
+  objects.rightWall.body = love.physics.newBody(world, 650, 650-50/2)
+  objects.rightWall.shape = love.physics.newRectangleShape(1, 650)
+  objects.rightWall.fixture = love.physics.newFixture(objects.rightWall.body, objects.rightWall.shape)
+ 
+  -- Här skapas en boll som utgår från mitten av skärmen, den är dynamisk och följer fysikaliska lagar
+  -- (den följer gravitationen och kan kollidera med andra objekt och marken)
   objects.ball = {}
-  -- Detta placerar bollen i centrum
-  objects.ball.body = love.physics.newBody(world, 650/2, 450/2, "dynamic")
-  -- Boll med radien 20px
-  objects.ball.shape = love.physics.newCircleShape(20)
-  -- Det här lägger till i bodyn och 1 på slutet ger den bollen densiteten 1
-  objects.ball.fixture = love.physics.newFixture(objects.ball.body,objects.ball.shape, 1)
+  objects.ball.body = love.physics.newBody(world, 650/2, 650/2, "dynamic")
+  objects.ball.shape = love.physics.newCircleShape(20) -- radie
+  objects.ball.fixture = love.physics.newFixture(objects.ball.body, objects.ball.shape, 1) -- densitet
+  objects.ball.fixture:setRestitution(1) -- det här gör att bollen studsar (allt under 1 gör att höjden avtar)
 
-  -- Gör bakgrunden vit
-  love.graphics.setBackgroundColor(1, 1, 1)
-  -- Det här bestämmer love-fönstrets dimensioner
+  -- Här ställer jag in bakgrundsfärg och LOVE2D-fönstrets storlek
+  love.graphics.setBackgroundColor(0.7, 0.7, 1)
   love.window.setMode(650, 650)
 end
 
 function love.update(dt)
-    -- Det här ger världen dess rörelse
-    world:update(dt)
+  world:update(dt)
+
+  -- Här skapas tangentbords event
+  if love.keyboard.isDown("right") then -- Pil höger, bollen flyttas höger
+    objects.ball.body:applyForce(400, 0)
+  elseif love.keyboard.isDown("left") then -- Pil vänster, bollen flyttas vänster
+    objects.ball.body:applyForce(-400, 0)
+  elseif love.keyboard.isDown("space") then -- Mellanslag, bollen återställs från centrum
+    objects.ball.body:setPosition(650/2, 650/2)
+    objects.ball.body:setLinearVelocity(0, 0) -- Det här sätter ursprungshastigheten till 0 för att motverka
+                                              -- en alldeles för stor förändring i kraft i riktningsbyte
+  end
 end
-   
-  function love.draw()
-    -- Gör plattformen svart
-    love.graphics.setColor(0, 0, 0)
-    -- Det här ritar polygonen som är marken
-    love.graphics.polygon("fill", objects.ground.body:getWorldPoints(objects.ground.shape:getPoints()))
-   
-    -- Gör bollen grön
-    love.graphics.setColor(0, 0.3, 0.4)
-    love.graphics.circle("fill", objects.ball.body:getX(),objects.ball.body:getY(), objects.ball.shape:getRadius())
+ 
+function love.draw()
+  -- Gör marken grön
+  love.graphics.setColor(.2, 0.3, 0)
+  love.graphics.polygon("fill", objects.ground.body:getWorldPoints(objects.ground.shape:getPoints()))
+  
+  -- Gör bollen röd
+  love.graphics.setColor(0.8, 0, 0.2)
+  love.graphics.circle("fill", objects.ball.body:getX(), objects.ball.body:getY(), objects.ball.shape:getRadius())
 end
