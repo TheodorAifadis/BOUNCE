2c9a3b65239012655d89d5d797a0567cf7e19253
TheodorAifadis <theodor.aifadis@skola.botkyrka.se>
2021-03-07 20:11:27 +0100

    commit-7

--- main.lua
+++ main.lua
@@ -1,61 +1,91 @@
 function love.load()
-  -- En meter i spelvärlden är 64 pixlar
   love.physics.setMeter(64)
-  -- Det här ger världen en allmän gravitation som endast verkar på det vertikala planet med 9.82
   world = love.physics.newWorld(0, 9.82*64, true)
+
+--[[
+  local begin_contact_callback = function (fixture_a, fixture_b, contact)
+    local objects_a = fixture_a:getUserData()
+    local objects_b = fixture_b:getUserData()
+    if objects_a.begin_contact then objects_a:begin_contact() end
+    if objects_b.begin_contact then objects_b:begin_contact() end
+  end
+
+  
+  world:setCallbacks(
+    begin_contact_callback,
+    nil,
+    nil,
+    nil
+  )
+--]]
  
-  objects = {} -- Här skapar jag en table där att fysiska kroppar kommer finnas
- 
-  -- Skapar marken som är en rektangel som placeras centralt långt ner på skärmen
+  objects = {}  
+
   objects.ground = {}
   objects.ground.body = love.physics.newBody(world, 650/2, 650-50/2)
   objects.ground.shape = love.physics.newRectangleShape(650, 50)
   objects.ground.fixture = love.physics.newFixture(objects.ground.body, objects.ground.shape)
 
   objects.leftWall = {}
-  objects.leftWall.body = love.physics.newBody(world, 0, 650-50/2)
-  objects.leftWall.shape = love.physics.newRectangleShape(1, 650)
+  objects.leftWall.body = love.physics.newBody(world, 1, 650-50/2)
+  objects.leftWall.shape = love.physics.newRectangleShape(50, 1300)
   objects.leftWall.fixture = love.physics.newFixture(objects.leftWall.body, objects.leftWall.shape)
 
   objects.rightWall = {}
-  objects.rightWall.body = love.physics.newBody(world, 650, 650-50/2)
-  objects.rightWall.shape = love.physics.newRectangleShape(1, 650)
+  objects.rightWall.body = love.physics.newBody(world, 649, 650-50/2)
+  objects.rightWall.shape = love.physics.newRectangleShape(50, 1300)
   objects.rightWall.fixture = love.physics.newFixture(objects.rightWall.body, objects.rightWall.shape)
- 
-  -- Här skapas en boll som utgår från mitten av skärmen, den är dynamisk och följer fysikaliska lagar
-  -- (den följer gravitationen och kan kollidera med andra objekt och marken)
+
   objects.ball = {}
   objects.ball.body = love.physics.newBody(world, 650/2, 650/2, "dynamic")
   objects.ball.shape = love.physics.newCircleShape(20) -- radie
   objects.ball.fixture = love.physics.newFixture(objects.ball.body, objects.ball.shape, 1) -- densitet
-  objects.ball.fixture:setRestitution(1) -- det här gör att bollen studsar (allt under 1 gör att höjden avtar)
+  objects.ball.fixture:setRestitution(1) -- studskoefficient
 
-  -- Här ställer jag in bakgrundsfärg och LOVE2D-fönstrets storlek
-  love.graphics.setBackgroundColor(0.7, 0.7, 1)
+  objects.triangle = {}
+  objects.triangle.body = love.physics.newBody(world, 300, 0, "dynamic")
+  objects.triangle.shape = love.physics.newPolygonShape(30, 30, 90, 30, 30, 90)
+  objects.triangle.fixture = love.physics.newFixture(objects.triangle.body, objects.triangle.shape, 1)
+ --[[
+  objects.triangle.begin_contact = function (self)
+    objects.triangle.fixture:destroy()
+  end
+--]]
+
+  love.graphics.setBackgroundColor(0, 0, 0)
   love.window.setMode(650, 650)
 end
 
 function love.update(dt)
   world:update(dt)
 
-  -- Här skapas tangentbords event
-  if love.keyboard.isDown("right") then -- Pil höger, bollen flyttas höger
+  if love.keyboard.isDown("right") then 
     objects.ball.body:applyForce(400, 0)
-  elseif love.keyboard.isDown("left") then -- Pil vänster, bollen flyttas vänster
+  elseif love.keyboard.isDown("left") then 
     objects.ball.body:applyForce(-400, 0)
-  elseif love.keyboard.isDown("space") then -- Mellanslag, bollen återställs från centrum
-    objects.ball.body:setPosition(650/2, 650/2)
-    objects.ball.body:setLinearVelocity(0, 0) -- Det här sätter ursprungshastigheten till 0 för att motverka
-                                              -- en alldeles för stor förändring i kraft i riktningsbyte
+  end
+
+function love.keypressed(k)
+  if k == 'escape' then
+    love.event.quit()
   end
 end
- 
+
+end
+
 function love.draw()
-  -- Gör marken grön
-  love.graphics.setColor(.2, 0.3, 0)
-  love.graphics.polygon("fill", objects.ground.body:getWorldPoints(objects.ground.shape:getPoints()))
+  love.graphics.setColor(.1, 0, 0)
+  love.graphics.polygon("fill", objects.leftWall.body:getWorldPoints(objects.leftWall.shape:getPoints()))
+
+  love.graphics.setColor(.1, 0, 0)
+  love.graphics.polygon("fill", objects.rightWall.body:getWorldPoints(objects.rightWall.shape:getPoints()))
   
-  -- Gör bollen röd
-  love.graphics.setColor(0.8, 0, 0.2)
+  love.graphics.setColor(0, .1, 0)
+  love.graphics.polygon("fill", objects.ground.body:getWorldPoints(objects.ground.shape:getPoints()))
+
+  love.graphics.setColor(1, 0, 0)
+  love.graphics.polygon("fill", objects.triangle.body:getWorldPoints(objects.triangle.shape:getPoints()))
+
+  love.graphics.setColor(1, 1, 1)
   love.graphics.circle("fill", objects.ball.body:getX(), objects.ball.body:getY(), objects.ball.shape:getRadius())
 end
